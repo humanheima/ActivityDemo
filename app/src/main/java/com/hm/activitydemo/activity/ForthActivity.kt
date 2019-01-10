fc6d417c13085ace38f5be5a97d5c7504fc899b6
humanheima <humanheima@gmail.com>
2019-01-10 18:30:28 +0800

    App temp files

---
+++ app/src/main/java/com/hm/activitydemo/activity/ForthActivity.kt
@@ -0,0 +1,13 @@
+package com.hm.activitydemo.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hm.activitydemo.R
+
+class ForthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forth)
+    }
+}
